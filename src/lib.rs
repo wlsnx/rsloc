07a7be68bba16390b67948378cb3275e306f49d3
zhuli <wanlisnx@gmail.com>
2023-01-31 16:41:20 +0800

    add print_count

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 #![feature(is_some_and)]
+
+use std::path::Path;
 mod counter;
-pub mod walker;
+mod walker;
+use walker::Walker;
+
+pub fn print_count<P>(path: P)
+where
+    P: AsRef<Path>,
+{
+    let mut walker = Walker::default();
+    walker.walk(path);
+    walker.print();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use rsloc::walker::Walker;
+use rsloc::print_count;
 use std::env;
 
 fn main() {
     let mut args = env::args();
     let _ = args.next(); // executable name
     let path = args.next().unwrap_or(".".to_string());
-    let mut walker = Walker::default();
-    walker.walk(path);
-    walker.print();
+    print_count(path);
 }
